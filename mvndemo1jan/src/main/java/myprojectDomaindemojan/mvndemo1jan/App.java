20b86da6132bd9755cdd8c33d0e37d963a44e3f7
Gopalakrishna530 <gopalkadiyam530@gmail.com>
2024-06-21 22:48:49 +0530

    this is my first commit

---
+++ mvndemo1jan/src/main/java/myprojectDomaindemojan/mvndemo1jan/App.java
@@ -0,0 +1,13 @@
+package myprojectDomaindemojan.mvndemo1jan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
